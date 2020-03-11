3eac78660bce97045ef621e6b5afb8b71cfede4d
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-03-10 21:25:53 -0700

    refactor packages

--- src/main/java/com/galvanize/hello/HelloRestControllerApplication.java
+++ src/main/java/com/galvanize/hello/HelloRestControllerApplication.java
@@ -1,4 +1,4 @@
-package com.example.hellorestcontroller;
+package com.galvanize.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/galvanize/hello/HelloRestControllerApplicationTests.java
+++ src/test/java/com/galvanize/hello/HelloRestControllerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.hellorestcontroller;
+package com.galvanize.hello;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
